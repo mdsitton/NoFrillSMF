7f58d19de99841c3c6d8c8e52e73a24095c67dbf
Matthew Sitton <matthewsitton@gmail.com>
2019-06-30 20:35:25 -0500

    Write first unit test!

---
+++ NoFrillSMF.Tests/SmfReadWriteTests.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.IO;
+using System.Linq;
+using System.Security.Cryptography;
+using Xunit;
+
+namespace NoFrillSMF.Tests
+{
+    public class SmfReadWriteTests
+    {
+        private byte[] GetHash(byte[] data)
+        {
+            using (SHA1 sha = SHA1.Create())
+            {
+                return sha.ComputeHash(data);
+            }
+        }
+
+        private byte[] GetHash(Stream data)
+        {
+            using (SHA1 sha = SHA1.Create())
+            {
+                return sha.ComputeHash(data);
+            }
+        }
+
+        [Theory]
+        [InlineData("midifiles/C3_Sound.mid")]
+        public void ReadWriteVerifyTest(string midiFile)
+        {
+            // Arrange
+            byte[] data = File.ReadAllBytes(midiFile);
+            MidiFile file = new MidiFile();
+
+            byte[] shaHash = GetHash(data);
+            byte[] outHash;
+
+            // Act
+            using (MemoryStream msIn = new MemoryStream(data))
+            using (MemoryStream msOut = new MemoryStream())
+            {
+
+                file.ReadData(msIn);
+                file.WriteData(msOut);
+
+                msOut.Position = 0;
+                outHash = GetHash(msOut);
+            }
+            // Assert
+            Assert.True(shaHash.SequenceEqual(outHash));
+
+        }
+    }
+}
