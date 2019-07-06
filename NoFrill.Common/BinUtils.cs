21bb75fd66fa7d49999bf59ac160431a3b154e24
Matthew Sitton <matthewsitton@gmail.com>
2019-07-05 20:40:30 -0500

    Add license reference to top of BinUtils

--- NoFrill.Common/BinUtils.cs
+++ NoFrill.Common/BinUtils.cs
@@ -1,4 +1,6 @@
-﻿using System;
+﻿// Copyright (c) 2019 Matthew Sitton <matthewsitton@gmail.com>
+// See LICENSE in the project root for license information.
+using System;
 using System.Runtime.CompilerServices;
 
 namespace NoFrill.Common
